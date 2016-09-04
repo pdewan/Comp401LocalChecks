915d156beb727cf97d6828b5773b8f9a0431547a
pdewan <dewan@cs.unc.edu>
2016-09-04 17:50:51 -0400

    Supporting multiple classes for MethodExecution

--- src/gradingTools/comp401f16/assignment1/testcases/IndexOfTest.java
+++ src/gradingTools/comp401f16/assignment1/testcases/IndexOfTest.java
@@ -1,22 +1,38 @@
 package gradingTools.comp401f16.assignment1.testcases;
 
+import org.junit.Test;
+
 import gradingTools.shared.testcases.MethodExecutionTest;
 
 
 public class IndexOfTest extends MethodExecutionTest {
+	
+	boolean tryScannerIterator;
 	
 	@Override
 	public Object[] getArgs() {
 		return new Object[] {"move 43 43 4 ", '3', 6} ;
 	}
 	
+	protected Object getTargetObject() {
+		return getTargetClass();
+	}
+	
 	
 	protected String getClassName() {
+//		if (tryScannerIterator) {
+//			return "ScanningIterator";
+//		}
 		return "main.Assignment1";
 	}
 	
 	protected String getMethodName() {
 		return "indexOf";
+	}
+	
+	@Override
+	protected boolean doTest() throws Throwable {
+		return super.doTest();
 	}
 	
 
