ecae2240c1a31d043cec7d1f872b73a4db5ef30d
pdewan <dewan@cs.unc.edu>
2016-11-29 09:33:14 -0500

    Checking colors of swing factory

--- src/gradingTools/comp401f16/assignment12/testcases/SwingTextFieldFactoryTestCase.java
+++ src/gradingTools/comp401f16/assignment12/testcases/SwingTextFieldFactoryTestCase.java
@@ -1,8 +1,10 @@
 package gradingTools.comp401f16.assignment12.testcases;
 
+import java.awt.Color;
 import java.lang.reflect.Method;
 
 import javax.swing.JTextField;
+import javax.swing.text.StyledEditorKit.ForegroundAction;
 
 import util.annotations.IsExtra;
 import util.annotations.MaxValue;
@@ -14,6 +16,10 @@ import gradingTools.shared.testcases.MethodExecutionTest;
 public class SwingTextFieldFactoryTestCase extends MethodExecutionTest{
 	protected static final double HAS_FACTORY_CLASS_CREDIT = 0.3;
 	protected static final String TEXT = "hello";
+	protected static final JTextField EXAMPLE_FIELD = new JTextField(TEXT);
+	protected static final Color DEFAULT_FOREGROUND = EXAMPLE_FIELD.getForeground();
+	protected static final Color DEFAULT_BACKGROUND = EXAMPLE_FIELD.getBackground();
+
 	protected String instantiatedTag() {
 		return "CustomSwingTextFieldFactory";
 	}	
@@ -34,6 +40,12 @@ public class SwingTextFieldFactoryTestCase extends MethodExecutionTest{
 		JTextField aJTextField = (JTextField) aMethod.invoke(aFactory, TEXT);
 		assertTrue ("Text property of " + aJTextField + " is not " + TEXT,
 				TEXT.equals(aJTextField.getText()));
+		Color aForeground = aJTextField.getForeground();
+		Color aBackground = aJTextField.getBackground();
+		assertTrue("Foreground color property of " + aJTextField + " is same as default value " + DEFAULT_FOREGROUND,
+				!aForeground.equals(DEFAULT_FOREGROUND));
+		assertTrue("Background color property of " + aJTextField + " is same as default value " + DEFAULT_BACKGROUND,
+				!aBackground.equals(DEFAULT_BACKGROUND));
 		return true;		
 	}
 }
