1888dec57108edc3b7c4a7c16e12b0d8c0d4fc15
dewan <dewan@cs.unc.edu>
2021-06-15 21:53:34 -0400

    Some initial A4 checks

---
+++ src/gradingTools/comp301ss21/assignment4/assertions/AssertingBridgeSceneDynamics.java
@@ -0,0 +1,15 @@
+package gradingTools.comp301ss21.assignment4.assertions;
+
+import gradingTools.comp401f16.assignment10.testcases.AssertingBridgeSceneDynamicTestCase;
+import tags301.Comp301Tags;
+import util.annotations.MaxValue;
+@MaxValue(72)
+public class AssertingBridgeSceneDynamics 
+	extends AssertingBridgeSceneDynamicTestCase {
+	String[] factoryClassTags = new String[] {Comp301Tags.FACTORY_CLASS};
+	protected String[] factoryClassTags() {
+		return factoryClassTags;
+	}
+	
+	
+}
--- src/tags301/Comp301Tags.java
+++ src/tags301/Comp301Tags.java
@@ -27,7 +27,14 @@ public class Comp301Tags extends ShapeTags {
 	public static final String BRIDGE_SCENE_CONTROLLER = "BridgeSceneController";
 	public static final String TABLE = "Table";
 	public static final String PROGRESS_BAR_CREATOR = "ProgressBarCreator";
-
+	public static final String ANIMATOR = "Animator";
+	public static final String ANIMATING_COMMAND = "AnimatingCommand";
+	public static final String LEGS = "Legs";
+	public static final String RESTRICTED_LINE = "RestrictedLine";
+	public static final String COORDINATED_ANIMATING_COMMAND = "CoordinatedAnimatingCommand";
+	public static final String COORDINATED_ANIMATOR = "Coordinated Animator";
+	public static final String COORDINATING_ANIMATING_COMMAND = "CoordinatingAnimatingCommand";
+	public static final String COORDINATING_ANIMATOR = "CoordinatedAnimator";
 
 	
 
