04112ad338d9513b4e2c50bd73c81fc4dfb30733
Prasun Dewan <dewan@cs.unc.edu>
2024-06-24 09:59:04 -0400

    Better explanation message for asbstraction waiting avatarts

--- src/gradingTools/comp301ss21/assignment4/coordination/AbstractionWaitingAvatars.java
+++ src/gradingTools/comp301ss21/assignment4/coordination/AbstractionWaitingAvatars.java
@@ -3,9 +3,11 @@ package gradingTools.comp301ss21.assignment4.coordination;
 import gradingTools.comp401f16.assignment.testInterfaces.TestBridgeScene;
 import gradingTools.comp401f16.assignment12.waitnotify.testcases.WaitingAvatarsAnimationTestCase;
 import tags301.Comp301Tags;
+import util.annotations.Explanation;
 import util.annotations.MaxValue;
 
 @MaxValue(30)
+@Explanation("Start the waiting animations of 4 knights, execute proceed all, and see if we subsequently get events from all four anumations")
 public class AbstractionWaitingAvatars extends AbstractionWaitingAvatarsAnimationTestCase {
 	String[] factoryClassTags = new String[] { Comp301Tags.FACTORY_CLASS };
 	protected String[] factoryClassTags() {
